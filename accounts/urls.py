07451b3e593689e172ad55e25579517984a0bba1
Mykelxu <mykelxu@gmail.com>
2025-09-24 22:17:34 -0400

    feat(profile): privacy controls + public profile page + private profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("profile/edit/", views.edit_profile, name="edit_profile"),
 
 
-    
+
     # password management
     path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
     path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
@@ -18,4 +18,8 @@ urlpatterns = [
     path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("me/", views.profile, name="profile"),  # owner view
+    path("edit/", views.edit_profile, name="edit_profile"),
+    path("u/<str:username>/", views.profile_detail, name="profile_detail"),  # public view
+
 ]
